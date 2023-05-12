76fb67b4f0f0baf309f1d2feb1a072de60f64193
Eduardo Leao <eduardo.leao@stoneage.com.br>
2023-05-12 17:04:21 -0300

    #2: adding airflow dag and tasks

--- flows/tasks/extract_latest_info.py
+++ flows/tasks/extract_latest_info.py
@@ -1,8 +1,8 @@
 import os
 from datetime import datetime as dt
 from dateutil.relativedelta import relativedelta
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 from tqdm import tqdm
 from binance import Client
 import pandas as pd
@@ -11,9 +11,8 @@ load_dotenv("../.env")
 
 BINANCE_API = os.environ.get('BINANCE_API', None)
 BINANCE_SECRET = os.environ.get('BINANCE_SECRET', None)
-TIMESPAN = 30
 
-def get_crypto_info(client, coin_name, exchange_info, interval='1d', start_date=dt.now()-relativedelta(days=TIMESPAN)):
+def get_crypto_info(client: Client, coin_name: str, exchange_info: dict, interval: str, start_date: dt.datetime):
   columns = [
     "open_time",
     "open_price",
@@ -45,10 +44,16 @@ def get_crypto_info(client, coin_name, exchange_info, interval='1d', start_date=
       return hist
     return None
 
-def main():
+def extract_latest_info(
+  test: bool = False,
+  coins: list = [],
+  timespan: int = 1,
+  interval: str = '1h',
+  output_path: str = "data/crypto_info.csv",
+):
 
-  test = False
-  coins = []
+  start_datetime = dt.now()-relativedelta(days=timespan)
+  start_date = dt(start_datetime.year, start_datetime.month, start_datetime.day)
 
   client = Client(BINANCE_API, BINANCE_SECRET)
   if test:
@@ -60,11 +65,11 @@ def main():
 
   dfs = []
   for coin in tqdm(coins):
-    crypto_info = get_crypto_info(client, coin, exchange_info)
+    crypto_info = get_crypto_info(client, coin, exchange_info, interval=interval, start_date=start_date)
     if crypto_info is not None:
       dfs.append(crypto_info)
 
-  pd.concat(dfs, ignore_index=True).to_csv("data/crypto_info.csv")
+  pd.concat(dfs, ignore_index=True).to_csv(output_path)
 
 if __name__ == '__main__':
-  main()
+  extract_latest_info()
---
+++ flows/tasks/load.py
@@ -0,0 +1,42 @@
+import sys
+sys.path.append("../")
+
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from db.schema import Kline, Coin
+
+def load(
+  connection_url: str = "postgresql://postgres:postgres@0.0.0.0:5432/crypto",
+  csv_path: str = "../data/crypto_info.csv"
+):
+  
+  dataframe = pd.read_csv(csv_path, index_col=0),
+  engine = create_engine(connection_url, echo=True)
+
+  with Session(engine) as session:
+    for row in dataframe.itertuples(index=False):
+      coin = Coin(
+        name = row.coin_name,
+        ticker = row.ticker,
+      )
+      kline = Kline(
+        coin = coin,
+        open_time = row.open_time,
+        open_price = row.open_price,
+        high_price = row.high_price,
+        low_price = row.low_price,
+        close_price = row.close_price,
+        volume = row.volume,
+        close_time = row.close_time,
+        quote_asset_volume = row.quote_asset_volume,
+        number_of_trades = row.number_of_trades,
+        taker_buy_base_asset_volume = row.taker_buy_base_asset_volume,
+        taker_buy_quote_asset_volume = row.taker_buy_quote_asset_volume,
+      )
+      session.add(kline)
+    session.commit()
+
+if __name__ == '__main__':
+  load()
